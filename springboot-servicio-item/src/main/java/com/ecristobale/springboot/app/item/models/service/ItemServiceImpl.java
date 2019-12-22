a9b6c93aebcef64396ce0dc72b22683d80de86fe
ecristobale <edu_ce_1988@hotmail.com>
2019-12-23 00:50:14 +0100

    Service layer methods implemented in item microservice

--- springboot-servicio-item/src/main/java/com/ecristobale/springboot/app/item/models/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/ecristobale/springboot/app/item/models/service/ItemServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ecristobale.springboot.app.item.models.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.ecristobale.springboot.app.item.models.Item;
+import com.ecristobale.springboot.app.item.models.Product;
 
 @Service
 public class ItemServiceImpl implements IItemService {
@@ -16,12 +21,16 @@ public class ItemServiceImpl implements IItemService {
 	
 	@Override
 	public List<Item> findAll() {
-		return null;
+		List<Product> products = Arrays.asList(clientRest.getForObject("http://localhost:8081/products", Product[].class));
+		return products.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
 	@Override
 	public Item findById(Long id, Integer units) {
-		return null;
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		Product product = clientRest.getForObject("http://localhost:8081/product/{id}", Product.class, pathVariables);
+		return new Item(product, units);
 	}
 
 }
