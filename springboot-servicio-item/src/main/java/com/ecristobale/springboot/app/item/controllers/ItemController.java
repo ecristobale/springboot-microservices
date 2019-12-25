174916d0385384f267f2f55d258cbc8869bbdc69
ecristobale <edu_ce_1988@hotmail.com>
2019-12-25 21:29:08 +0100

    - Test with Postman calls with RestTemplate

--- springboot-servicio-item/src/main/java/com/ecristobale/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/ecristobale/springboot/app/item/controllers/ItemController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class ItemController {
 	private Environment env;
 
 	@Autowired
-	@Qualifier("serviceFeign")//("serviceRestTemplate")
+	@Qualifier("serviceRestTemplate")//("serviceFeign")
 	private IItemService itemService;
 	
 	@GetMapping("/list")
@@ -88,7 +89,7 @@ public class ItemController {
 		return itemService.save(product);
 	}
 	
-	@PostMapping("/update/{id}")
+	@PutMapping("/update/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Product update(@RequestBody Product product, @PathVariable Long id) {
 		return itemService.update(product, id);
