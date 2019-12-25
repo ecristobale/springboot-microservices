7f2abc9045668c16ae6401681d4add5072481185
ecristobale <edu_ce_1988@hotmail.com>
2019-12-25 13:57:10 +0100

    - CRUD: created save (POST) operation in product microservice. - Tested in postman just for product microservice

--- springboot-servicio-productos/src/main/java/com/ecristobale/springboot/app/productos/models/service/IProductService.java
+++ springboot-servicio-productos/src/main/java/com/ecristobale/springboot/app/productos/models/service/IProductService.java
@@ -8,4 +8,8 @@ public interface IProductService {
 
 	public List<Product> findAll();
 	public Product findById(Long id);
+	
+	public Product save(Product product);
+	
+	public void deleteById(Long id);
 }
