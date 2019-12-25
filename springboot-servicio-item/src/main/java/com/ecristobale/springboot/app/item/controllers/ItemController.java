67a460048e3ffe7fb54f3a1c46ae97fc5d09ae08
ecristobale <edu_ce_1988@hotmail.com>
2019-12-25 01:43:17 +0100

    -Spring actuator added at Items microservice for update at the moment the info retrieved by the properties. - Configured an alternative way to retrieve the file from github repository

--- springboot-servicio-item/src/main/java/com/ecristobale/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/ecristobale/springboot/app/item/controllers/ItemController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ import com.ecristobale.springboot.app.item.models.Product;
 import com.ecristobale.springboot.app.item.models.service.IItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemController {
 	
