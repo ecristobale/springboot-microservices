93ae710fca9ce611e240812a3ba6722bf868c225
ecristobale <edu_ce_1988@hotmail.com>
2019-12-29 21:44:47 +0100

    Created the schema and the classes of 2 entities with a manytomany relationship Configured H2

---
+++ springboot-servicio-usuarios/src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS usuarios_roles;
+
+CREATE TABLE roles(
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR (30) UNIQUE
+);
+
+CREATE TABLE usuarios(
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR (255),
+   apellido VARCHAR (255),
+   enabled BOOLEAN,
+   username VARCHAR (20) UNIQUE,
+   password VARCHAR (60) UNIQUE,
+   email VARCHAR (100) UNIQUE
+);
+
+CREATE TABLE usuarios_roles(
+   usuario_id BIGINT NOT NULL,
+   role_id BIGINT NOT NULL,
+   PRIMARY KEY (usuario_id, role_id),
+   FOREIGN KEY (role_id) references roles,
+   FOREIGN KEY (usuario_id) references usuarios
+);
