2fadd8cb8abe448970594582cbec7816c298681c
ecristobale <edu_ce_1988@hotmail.com>
2019-12-30 02:21:23 +0100

    - New project commons usuarios created. - Updated dependencies of usuarios to point commons usuarios.

--- springboot-servicio-usuarios-commons/src/main/java/com/ecristobale/springboot/app/usuarios/commons/models/entity/Usuario.java
+++ springboot-servicio-usuarios-commons/src/main/java/com/ecristobale/springboot/app/usuarios/commons/models/entity/Usuario.java
@@ -1,4 +1,4 @@
-package com.ecristobale.springboot.app.usuarios.models.entity;
+package com.ecristobale.springboot.app.usuarios.commons.models.entity;
 
 import java.io.Serializable;
 import java.util.List;
