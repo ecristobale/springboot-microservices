231ed98cedd4c9b3f90f7293e4e9663a5716b6ae
ecristobale <edu_ce_1988@hotmail.com>
2019-12-30 17:23:49 +0100

    - Spring Security configuration implemented by creating one Feign Client that is going to call to servicio-usuarios - UsuarioService, which implements UserDetailsService and overrides loadUserByUsername. - SpringSecurityConfiguration, conf class that creates beans and extends WebSecurityConfigurerAdapter

---
+++ springboot-servicio-oauth/src/main/java/com/ecristobale/springboot/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,14 @@
+package com.ecristobale.springboot.app.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ecristobale.springboot.app.usuarios.commons.models.entity.Usuario;
+
+@FeignClient(name="servicio-usuarios")
+public interface UsuarioFeignClient {
+
+	@GetMapping("/usuarios/search/buscar-username")
+	public Usuario findByUsername(@RequestParam String usrname);
+}
