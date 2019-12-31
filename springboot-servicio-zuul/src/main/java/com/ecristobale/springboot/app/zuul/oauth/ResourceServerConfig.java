96b017dbc3f8161e3420733b8ca9ecbd4c8a1322
ecristobale <edu_ce_1988@hotmail.com>
2019-12-31 04:59:21 +0100

    - Added CORS compatibility for clients like angular.

--- springboot-servicio-zuul/src/main/java/com/ecristobale/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ springboot-servicio-zuul/src/main/java/com/ecristobale/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,8 +1,12 @@
 package com.ecristobale.springboot.app.zuul.oauth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +14,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -32,11 +40,34 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 					"/api/items/show/{id}/{units}", 
 					"/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
 			.antMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/**").hasRole("ADMIN")
-			.anyRequest().authenticated();
+			.anyRequest().authenticated()
+			.and().cors().configurationSource(corsConfigurationSource());
 //			.antMatchers(HttpMethod.POST, "/api/productos/create", "/api/items/create", "/api/usuarios/usuarios").hasRole("ADMIN")
 //			.antMatchers(HttpMethod.PUT, "/api/productos/update/{id}", "/api/items/update/{id}", "/api/usuarios/usuarios/{id}").hasRole("ADMIN")
 //			.antMatchers(HttpMethod.DELETE, "/api/productos/delete/{id}", "/api/items/delete/{id}", "/api/usuarios/usuarios/{id}").hasRole("ADMIN");
 		super.configure(http);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.addAllowedOrigin("*");
+		//corsConfig.addAllowedOrigin("http://localhost:4200");
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 	@Bean
