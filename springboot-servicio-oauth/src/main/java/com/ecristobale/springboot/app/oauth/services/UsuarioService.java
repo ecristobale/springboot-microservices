1c2f4a25483f0d5d6c4f6b125ea0e38a995d1d65
ecristobale <edu_ce_1988@hotmail.com>
2020-01-01 18:52:45 +0100

    - Handling message returned after bad login attemp.

--- springboot-servicio-oauth/src/main/java/com/ecristobale/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/ecristobale/springboot/app/oauth/services/UsuarioService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import com.ecristobale.springboot.app.oauth.clients.UsuarioFeignClient;
 import com.ecristobale.springboot.app.usuarios.commons.models.entity.Usuario;
 
+import feign.FeignException;
+
 @Service
 public class UsuarioService implements UserDetailsService, IUsuarioService {
 	
@@ -27,22 +29,24 @@ public class UsuarioService implements UserDetailsService, IUsuarioService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Usuario usuario = client.findByUsername(username);
 		
-		if(usuario== null) {
+		try {
+			Usuario usuario = client.findByUsername(username);
+			
+			List<GrantedAuthority> authorities = usuario.getRoles()
+					.stream()
+					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+					.peek(authority -> log.info("Role: " + authority.getAuthority()))
+					.collect(Collectors.toList());
+			log.info("Usuario autenticado: " + username);
+			
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(),
+					true, true, true, authorities);
+		
+		} catch (FeignException e) {
 			log.error("Error en login, no exist el usuario '" + username + "' en el sistema");
 			throw new UsernameNotFoundException("Error en login, no exist el usuario '" + username + "' en el sistema");
 		}
-		
-		List<GrantedAuthority> authorities = usuario.getRoles()
-				.stream()
-				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
-				.peek(authority -> log.info("Role: " + authority.getAuthority()))
-				.collect(Collectors.toList());
-		log.info("Usuario autenticado: " + username);
-		
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(),
-				true, true, true, authorities);
 	}
 
 	@Override
